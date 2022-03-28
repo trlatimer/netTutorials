8e1ec33128abd776b40fb2a6dd880ba9948f6518
trlatimer <trlatimer95@gmail.com>
2022-03-28 11:46:06 -0700

    Restarted with .net 5.0 as target framework

--- RazorPagesMovie/RazorPagesMovie/Pages/Error.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Error.cshtml.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RazorPagesMovie.Pages
 {
@@ -8,7 +13,7 @@ namespace RazorPagesMovie.Pages
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
